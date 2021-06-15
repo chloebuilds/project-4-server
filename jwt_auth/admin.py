538a56556b28f729dd47962bc069cc5276b882c9
Chloe Brown <chloemail@protonmail.com>
2021-06-15 21:20:38 +0100

    updates to users and jwt_auth

--- jwt_auth/admin.py
+++ jwt_auth/admin.py
@@ -1,34 +1,10 @@
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 
-from .forms import UserChangeForm, UserChangeForm
+# Register your models here.
 
 User = get_user_model()
 
-
-# class UserAdmin(BaseUserAdmin):
-#     form = UserChangeForm
-#     fieldsets = (
-#         (None, {'fields': ('email', 'password', )}),
-#         (_('Personal info'), {'fields': ('first_name', 'last_name')}),
-#         (_('Permissions'), {'fields': ('is_active', 'is_staff',
-#         'is_superuser', 'groups', 'user_permissions')}),
-#         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-#         (_('user_info'), {'fields': ('native_name', 'phone_no')}),
-#     )
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide', ),
-#             'fields': ('email', 'password1', 'password2'),
-#         }),
-#     )
-#     list_display = ['email', 'first_name', 'last_name',
-#                     'is_staff', "native_name", "phone_no"]
-#     search_fields = ('email', 'first_name', 'last_name')
-#     ordering = ('email', )
 
 
 admin.site.register(User)
